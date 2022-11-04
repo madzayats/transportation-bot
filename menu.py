f6f6dd56fe86724d0bbd1dba71cd095c842ac4f5
Alexander <shigalinskiy.pivozavr@yandex.ru>
2022-11-04 23:45:43 +0300

    Add FAQ

--- menu.py
+++ menu.py
@@ -1,9 +1,19 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove, InlineKeyboardButton, \
+    InlineKeyboardMarkup
 
-calc_trans_button = KeyboardButton('/Рассчитать перевозку')
+# main menu
+
+calc_trans_button = KeyboardButton('Рассчитать перевозку')
 faq_button = KeyboardButton('FAQ - Вопросы и ответы')
 hours_button = KeyboardButton('Адреса и режим работы')
 
 main_menu_kb = ReplyKeyboardMarkup(resize_keyboard=True)
 
 main_menu_kb.add(calc_trans_button).add(faq_button).add(hours_button)
+
+
+
+# technical support
+
+call_tech_support = InlineKeyboardButton('🆘Написать в техническую поддержку🆘', callback_data='call_support')
+call_support = InlineKeyboardMarkup().add(call_tech_support)
